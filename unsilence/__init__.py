c71bb88ce17abd1bd1846bc0b8984141e2c57db9
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-09-15 19:51:20 -0400

    Delete unsilence directory

--- unsilence/__init__.py
+++
@@ -1,3 +0,0 @@
-from unsilence.Unsilence import Unsilence
-from unsilence.lib.intervals.Interval import Interval
-from unsilence.lib.intervals.Intervals import Intervals
--- unsilence/command_line/TerminalSupport.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def repair_console():
-    if os.name == "posix":
-        os.system("stty sane")
